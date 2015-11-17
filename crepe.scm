5f77ed73c3d5320e9f29d94afd3e24874a73dd04
Kooda <kooda@upyum.com>
2015-11-17 17:18:18 +0100

    Start the refactoring

--- crepe.scm
+++ crepe.scm
@@ -1,5 +1,6 @@
 (use
   cairo-utils
+  matchable
   sdl2
   (prefix sdl2-image img:)
   )
@@ -22,19 +23,13 @@
 (init!)
 (assert (member 'png (img:init! '(png))))
 
-(define crepe-down-surface (img:load "graph/down.png"))
-(define crepe-up-surface (img:load "graph/up.png"))
-(define crepe-left-surface (img:load "graph/left.png"))
-(define crepe-right-surface (img:load "graph/right.png"))
-(define crepe-stick-surface (img:load "graph/sticky-full.pngxb"))
-(define background-surface (img:load "graph/background.png"))
-
-(assert (and crepe-down-surface
-             crepe-up-surface
-             crepe-left-surface
-             crepe-right-surface
-             crepe-stick-surface
-             background-surface))
+(define crepe-down-surface (assert (img:load "graph/down.png")))
+(define crepe-up-surface (assert (img:load "graph/up.png")))
+(define crepe-left-surface (assert (img:load "graph/left.png")))
+(define crepe-right-surface (assert (img:load "graph/right.png")))
+(define crepe-stick-surface (assert (img:load "graph/sticky-full.png")))
+(define crepe-unstick-surface (assert (img:load "graph/unstick.png")))
+(define background-surface (assert (img:load "graph/background.png")))
 
 (define win (create-window! "Crepes-party-hard-yolo-swag 2015"
                             'undefined 'undefined
@@ -44,22 +39,6 @@
 
 (init-utils! win)
 
-(define (draw-grid)
-  (draw-lines)
-  (draw-columns))
-
-(define (draw-lines)
-  (let loop ((y 0))
-    (unless (>= y (* +lines-number+ +line-space+))
-      (draw-line 0 y +width+ y color: white)
-      (loop (+ y +line-space+)))))
-
-(define (draw-columns)
-  (let loop ((x 0))
-    (unless (>= x (* +columns-number+ +column-space+))
-      (draw-line x 0 x +height+ color: white)
-      (loop (+ x +column-space+)))))
-
 (define (draw-player player)
   (let ((w 120)
         (h 300))
@@ -67,53 +46,47 @@
     (+ (* player +column-space+) (/ w 2))
     (- (* (sub1 +lines-number+) +line-space+) (/ h 2))
     w h
-    ;; +column-space+ +line-space+
     red)))
 
-(define (get-crepe-surface crepe clock)
-  (with-slots crepe crepe (column line state speed)
-    (cond
-     ((eq? state 'ascend) crepe-up-surface)
-     ((eq? state 'stick) crepe-stick-surface)
-     ((< (sin (/ clock speed)) -0.4) crepe-left-surface)
-     ((and (> (sin (/ clock speed)) -0.4) (< (sin (/ clock speed)) 0.4)) crepe-down-surface)
-     ((> (sin (/ clock speed)) 0.4) crepe-right-surface))))
+(define (crepe-surface state wiggle)
+  (match state
+    (($ ascend-state)
+     crepe-up-surface)
+    (($ stick-state)
+     crepe-stick-surface)
+    (($ descend-state speed time)
+     (cond
+      ((< wiggle -0.4) crepe-left-surface)
+      ((> 0.4 wiggle -0.4) crepe-down-surface)
+      ((> wiggle 0.4) crepe-right-surface)))))
 
 (define ((draw-crepe clock) crepe)
-  (with-slots crepe crepe (column line state speed)
-    (let* ((color (case state ((descend) yellow) ((ascend) blue) ((stick) violet)))
-	   (speed (case state ((ascend) +ascend-speed+) (else speed)))
-	   (x (* (quotient +column-space+ 4) (sin (/ clock speed))))
-	   (surface (get-crepe-surface crepe clock)))
-      (blit-surface!
-       surface
-       #f
-       win-surface
-       (make-rect
-        (round
-         (+ (/ +column-space+ 2)
-            (- (* column +column-space+) (/ (surface-w surface) 2))
-            (if (eq? 'descend state)
-                x
-                0)))
-        (round
-         (+ (/ +line-space+ 2)
-            (- (* line +line-space+) (/ (surface-h surface) 2))
-            (crepe-tweening crepe clock))))))))
+  (let* ((state (crepe-state crepe))
+         (column (crepe-column crepe))
+         (speed (match state (($ descend-state speed) speed) (else +ascend-speed+)))
+         (wiggle (sin (/ clock (/ speed 9))))
+         (surface (crepe-surface state wiggle)))
+    (blit-surface!
+     surface
+     #f
+     win-surface
+     (make-rect
+      (round
+       (+ (/ +column-space+ 2)
+          (- (* column +column-space+) (/ (surface-w surface) 2))
+          (if (descend-state? state) (* (quotient +column-space+ 4) wiggle) 0)))
+      (round
+       (crepe-y state clock))))))
 
-(define (crepe-tweening crepe clock)
-  (with-slots crepe crepe (state line speed last-time)
-    (let ((coef (case state ((ascend) -1) ((descend) +1) ((stick) 0))))
-      (* coef
-         (/ (- clock last-time)
-            (if (eq? state 'descend) speed +ascend-speed+))
-         +line-space+))))
+(define (crepe-y state clock)
+  (+ +highest-point+
+     (* (height clock state)
+        (- +lowest-point+ +highest-point+))))
 
 (define (draw-game player lives score board clock)
   (blit-surface! background-surface #f win-surface #f)
   (draw-player player)
   (for-each (draw-crepe clock) board)
-  ;; (draw-grid)
   (font-size 16)
   (font-color black)
   (text (- +width+ 10) 20 (sprintf "Lives: ~A  Score: ~A" lives score) align: #:right)
@@ -144,28 +117,31 @@
 
 (define (main-loop player lives score board)
   (let* ((clock (get-ticks))
-         (events (collect-events!))
-         (direction (get-direction (find keydown-event? events)))
-         (speed-interval (sub-speed-interval +initial-speed-interval+ score))
-         (new-board (move-crepes clock player board speed-interval))
-         (lives-lost (count crepe-outside-board? new-board))
-         (score-increment (compute-score board new-board)))
+         (new-player (move-player player (get-direction (find keydown-event? (collect-events!)))))
+         (new-board (map (lambda (c) (compute-crepe clock new-player score c)) board))
+         (score-increment (compute-score board new-board))
+         (lives-lost (count (lambda (c) (outside-board? clock c)) new-board)))
     (draw-game player lives score board clock)
-    (unless (handle-repl)
-      (main-loop (move-player player direction)
+    (unless (dead? lives)
+      (main-loop new-player
                  (- lives lives-lost)
                  (+ score score-increment)
-                 (revive-crepes new-board)))))
+                 (revive-crepes clock new-board)))))
 
-(define (handle-repl)
-  (if (char-ready?)
-   (let ((sexp (read)))
-     (if (eof-object? sexp)
-         #t
-         (begin
-           (write (eval sexp))
-           (newline)
-           #f)))
-   #f))
+(define (compute-crepe clock player score crepe)
+  (let ((state (crepe-state crepe)))
+    (update-crepe
+     crepe
+     state: (cond
+             ((and (stick-state? state) (should-fall?))
+              (make-descend-state time: clock speed: (random-speed score)))
+             ((and (ascend-state? state) (>= clock (+ (ascend-state-time state) +ascend-speed+)))
+              (make-stick-state))
+             ((and (descend-state? state)
+                   (= player (crepe-column crepe))
+                   (within-catch-range? clock (descend-state-time state) (descend-state-speed state)))
+              (make-ascend-state time: clock))
+             (else
+              state)))))
 
-(start-game)
+;(start-game)
